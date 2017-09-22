c458aa847105bb25101de608e42dabfe6ab50398
C3D1 <cedric_schnider@outlook.com>
2017-09-22 10:42:14 +0200

    Add project

---
+++ 2-Factor_Auth/2-Factor_Auth/Controller/AuthentificationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2_Factor_Auth.Controller
+{
+    public class AuthentificationController : System.Web.Mvc.Controller
+    {
+        // GET: Authentification
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 2-Factor_Auth/2-Factor_Auth/Model/AuthentificationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OtpNet;
+
+namespace _2_Factor_Auth.Model
+{
+    public class AuthentificationModel
+    {
+        public byte[] Secret { get; set; }
+        public Totp Timestamp { get; set; }
+    }
+}
