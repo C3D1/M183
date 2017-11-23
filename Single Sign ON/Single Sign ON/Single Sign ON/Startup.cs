19bf71207e383ebd3f51ff221bd182c7ece60efc
C3D1 <cedric_schnider@outlook.com>
2017-11-23 13:21:35 +0100

    Single Sign On started

---
+++ Single Sign ON/Single Sign ON/Single Sign ON/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Single_Sign_ON.Startup))]
+namespace Single_Sign_ON
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
