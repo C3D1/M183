ebb74ae6ae7aba3191970d9c8e5226cce2722bdb
C3D1 <cedric_schnider@outlook.com>
2017-11-23 13:46:55 +0100

    Finished Single Sign On (not sure if it's working 100%)

--- Single Sign ON/Single Sign ON/Single Sign ON/Controllers/HomeController.cs
+++ Single Sign ON/Single Sign ON/Single Sign ON/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -29,7 +32,26 @@ namespace Single_Sign_ON.Controllers
 
         public JsonResult SSTokenSignIn()
         {
-            return Json("");
+            var id_token = Request["idtoken"];
+            var request = (HttpWebRequest)WebRequest.Create("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + id_token);
+
+            var postData = "id_token" + id_token;
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse) request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return Json(responseString);
         }
     }
 }
